a410758d034c22fe5311b57baef39c9de105901f
Sreenath <sreenathsree15@gmail.com>
2025-05-27 14:14:13 +0530

    main button added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <button>main-button</button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
